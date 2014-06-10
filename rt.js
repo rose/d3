6ffd9d3fc83984ccb7981226021cc196582c1d5a
Rose Ames <rose@happyspork.com>
2014-06-10 15:08:04 -0400

    add colours

--- rt.js
+++ rt.js
@@ -1,41 +1,73 @@
-// window.innerHeight / Width
-var words = ["hello", "world", "this", "is", "a", "text"]
-var viewport = 
-  d3.select("#d3")
+// construct data
+
+var labels = ["hello", "world", "this", "is", "a", "text"];
+var node_data = [];
+
+for (var i = 0; i < labels.length; i++) {
+  node_data.push({ 
+    label: labels[i],
+    size: labels[i].length * 5,
+    colour: "#" 
+      + Math.floor(Math.random() * 4 + 4).toString(16)
+      + Math.floor(Math.random() * 6 + 10).toString(16)
+      + Math.floor(Math.random() * 4 + 4).toString(16),
+    display: false
+  });
+}
+
+
+// make viewports
+
+var tree_port = 
+  d3.select("#tree")
   .append("svg")
   .attr("width", 400)
   .attr("height", 400)
-  .style("background-color", "yellow");
+  .style("background-color", "#cceeff")
+  .style("display", "inline-block");
+
+var poem_port = 
+  d3.select("#poem")
+  .append("svg")
+  .attr("width", 200)
+  .attr("height", 200)
+  .style("background-color", "#ffff88")
+  .style("display", "inline-block");
+  
+
+// create display elements
 
 var circles = 
-  viewport
+  tree_port
   .selectAll(".text_circle")
-  .data(words)
+  .data(node_data)
   .enter()
   .append("circle");
 
 var text = 
-  viewport
+  tree_port
   .selectAll(".text_content")
-  .data(words)
+  .data(node_data)
   .enter()
   .append("text")
-  .text(function(d,i) { return d; } )
+  .text(function(d,i) { return d.label; } )
   .attr("y", function(d,i) {
     return i * 50 + 30;
   })
   .attr("x", 25)
 
-var style = 
+var style_circles = 
   circles
   .attr("cy", function(d,i) {
     return i * 50 + 30;
   })
   .attr("cx", 25)
   .attr("r", function(d,i) {
-    return d.length * 5;
+    return d.size;
   })
-  .style("fill", "green");
+  .style("fill", function(d) {
+    return d.colour;
+  });
   
   
 /*
