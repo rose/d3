64ab4c5991b1494258e97abc5fc4677b5ae8c7b7
Rose Ames <rose@happyspork.com>
2014-06-10 01:07:54 -0400

    add text

--- rt.js
+++ rt.js
@@ -1,18 +1,18 @@
 // window.innerHeight / Width
-
+var words = ["hello", "world", "this", "is", "a", "text"]
 var viewport = 
   d3.select("#d3")
   .append("svg")
   .attr("width", 400)
   .attr("height", 400)
-  .style("background-color", "yellow")
+  .style("background-color", "yellow");
 
 var circles = 
   viewport
   .selectAll(".text_circle")
-  .data(["hello", "world", "this", "is", "a", "text"])
+  .data(words)
   .enter()
-  .append("circle")
+  .append("circle");
 
 var style = 
   circles
@@ -23,4 +23,23 @@ var style =
   .attr("r", function(d,i) {
     return d.length * 5;
   })
-  .style("fill", "green")
+  .style("fill", "green");
+  
+var text = viewport.selectAll(".text")
+  .data(words)
+  .enter()
+  .append("text")
+  .attr("y", function(d,i) {
+    return i * 50 + 30;
+  })
+  .attr("x", 25)
+  .text(function(d) { return d; })
+  .attr("fg", "red")
+
+//var effects = 
+  //circles
+  //.on("mouseover", function(d, i) {
+    //d3.select(this)
+    //.insert("text")
+    //.text(d);
+  //});
